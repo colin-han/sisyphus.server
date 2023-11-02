81b987e1fc2dcbb42fd0a488321f3bf4f4189a15
Colin <biz@colinhan.info>
2023-11-02 20:23:54 +0800

    Add missing field.

--- src/main/java/info/colinhan/sisyphus/server/dto/ProgramInfo.java
+++ src/main/java/info/colinhan/sisyphus/server/dto/ProgramInfo.java
@@ -18,12 +18,16 @@ public class ProgramInfo {
     private long id;
     private String name;
     private List<String> variables;
+    private String createdBy;
+    private Timestamp createdAt;
     private Timestamp updatedAt;
 
     public static ProgramInfo from(ProgramEntity program) {
         return new ProgramInfo(program.getId(),
                 program.getName(),
                 Collections.emptyList(),
+                program.getCreatedBy(),
+                program.getCreatedAt(),
                 program.getUpdatedAt());
     }
 }
